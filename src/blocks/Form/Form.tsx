bce0bbe2d47643beaffa452909d26e5be2cecea4
Kris Papercut <krispcut@gmail.com>
2025-10-30 02:26:04 +0200

    v0.0.2

--- src/blocks/Form/Form.tsx
+++ src/blocks/Form/Form.tsx
@@ -1,59 +1,61 @@
-import React, {useCallback, FormEvent, ComponentType, PropsWithChildren} from "react";
+import React, {useRef, useCallback, FormEvent, ElementType, ComponentType, PropsWithChildren} from "react";
 import {
     useForm,
     FormProvider,
-    Resolver,
+    UseFormProps,
     FieldValues,
-    Mode,
-    DefaultValues
+    SubmitHandler,
+    SubmitErrorHandler
 } from "react-hook-form";
 
-
-export type FormProps<T extends FieldValues = FieldValues> = PropsWithChildren<{
-    as?: ComponentType<{
-        className?: string;
-        onSubmit: (e: FormEvent) => void;
-    }>;
+type FormComponentProps = {
     className?: string;
-    mode?: Mode;
-    resolver?: Resolver<T>;
-    values?: T;
-    defaultValues?: DefaultValues<T>;
-    onSubmit?: (data: T) => void;
-    onError?: (err: unknown) => void;
-}>;
+    onSubmit?: (e: FormEvent) => void;
+};
 
-export const Form = <T extends FieldValues = FieldValues>(props: FormProps<T>) => {
+export type FormProps<T extends FieldValues = FieldValues, C = any> = PropsWithChildren<
+    UseFormProps<T, C> & {
+        as?: ElementType<FormComponentProps> | ComponentType<FormComponentProps>;
+        className?: string;
+        onSubmit?: SubmitHandler<T>;
+        onInvalid?: SubmitErrorHandler<T>;
+    }
+>;
+
+export const Form = <T extends FieldValues = FieldValues, C = any>(props: FormProps<T, C>) => {
     const {
         as: Component = "form",
         className,
-        mode,
-        resolver,
-        values,
-        defaultValues,
         children,
         onSubmit,
-        onError
+        onInvalid,
+        ...rest
     } = props;
 
-    const formProps = useForm({
-        mode,
-        resolver,
-        values,
-        defaultValues
-    });
+    const formProps = useForm<T, C>(rest),
+          submitRef = useRef(onSubmit),
+          invalidRef = useRef(onInvalid);
 
-    const handleSubmit = useCallback((data: any) => {
-        if(onSubmit) {
-            onSubmit(data);
+    submitRef.current = onSubmit;
+    invalidRef.current = onInvalid;
+
+    const handleSubmit: SubmitHandler<T> = useCallback((data: T, event) => {
+        if(submitRef.current) {
+            submitRef.current(data, event);
         }
-    }, [onSubmit]);
+    }, []);
+
+    const handleInvalid: SubmitErrorHandler<T> = useCallback((errors, event) => {
+        if(invalidRef.current) {
+            invalidRef.current(errors, event);
+        }
+    }, []);
 
     return (
-        <FormProvider {...formProps}>
+        <FormProvider<T> {...formProps}>
             <Component
               className={className}
-              onSubmit={formProps.handleSubmit(handleSubmit, onError)}>
+              onSubmit={formProps.handleSubmit(handleSubmit, handleInvalid)}>
                 {children}
             </Component>
         </FormProvider>
